683121c8479f8664b05c8f062db286e86381a9d5
Valerio Cicchella <valerio.cicchella@gmail.com>
2020-04-12 15:28:32 +0200

    Revert "Function description are more clear"
    This reverts commit c3ff05a992f829b80b987d57105e793009ba32a2.

--- bikeshare.py
+++ bikeshare.py
@@ -122,7 +122,7 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays the mean of the total and average trip duration."""
+    """Displays statistics on the total and average trip duration."""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
@@ -139,7 +139,7 @@ def trip_duration_stats(df):
     print('-'*40)
 
 def user_stats(df):
-    """Displays max min mode on bikeshare users."""
+    """Displays statistics on bikeshare users."""
 
     print('\nCalculating User Stats...\n')
     start_time = time.time()
